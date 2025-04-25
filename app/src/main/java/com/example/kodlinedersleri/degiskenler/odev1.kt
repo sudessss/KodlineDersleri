98a81b06d85e3c93f155a3b388bcc35c6805dd81
sudessss <sudesenturk77@gmail.com>
2025-04-25 13:24:34 +0300

    odev1

---
+++ app/src/main/java/com/example/kodlinedersleri/degiskenler/odev1.kt
@@ -0,0 +1,154 @@
+package com.example.kodlinedersleri.degiskenler
+
+fun main () {
+    var sehir = "Istanbul"
+    println("sehir : $sehir")
+
+    val ulke = "Turkiye"
+    println("ulke : $ulke")
+
+    var telefon = "535 394 42 10"
+    println("telefon : $telefon")
+    
+    val posta_kodu = "14414"
+    println("postakodu = $posta_kodu")
+    
+    var email = "sudesenturk77@gmail.com"
+    println("email : $email")
+    
+    var meslek = "bilgisayarMuhendisligi"
+    println("meslek : $meslek")
+    
+    var stokMiktari = "var"
+    println("stokMiktari : $stokMiktari")
+    
+    var musteriAdi = "Sude Senturk"
+    println("musteriAdi : $musteriAdi")
+
+    var bakiye = 12
+    println("bakiye : $bakiye")
+
+    val dogumGunu = "21/08/2005"
+    println("dogumGunu : $dogumGunu")
+
+    var maas = 0
+    println("maas : $maas")
+
+    var medeniDurumu = "Bekar"
+    println("medeniDurumu : $medeniDurumu")
+
+    var urunYorumu = "harika kumas"
+    println("urunYorumu : $urunYorumu")
+
+    val odemeTarihi = "24.04.2025"
+    println("odemeTarihi : $odemeTarihi")
+
+    val odeme = "pesin"
+    println("odeme : $odeme")
+
+    var siparisAdeti = 1000
+    println("siparisAdeti : $siparisAdeti")
+
+    val arabaModeli = "MercedesGKasa"
+    println("arabaModeli : $arabaModeli")
+
+    var kitapAdi = "SucveCeza"
+    println("kitapAdi: $kitapAdi")
+
+    val yayinlanmaTarihi = "24.04.2025"
+    println("yayinlanmaTarihi : $yayinlanmaTarihi")
+
+    var indirimMiktari = "%20"
+    println("indirimMiktari : $indirimMiktari")
+
+    var odaSayisi = "2+1"
+    println("odaSayisi: $odaSayisi")
+
+    var enlem = "enlem"
+    println("enlem: $enlem")
+
+    var boylam = "boylam"
+    println("boylam: $boylam")
+
+    var urunAdi = "kaban"
+    println("urunAdi: $urunAdi")
+
+    var yemekFiyati = 430
+    println("yemekFiyati: $yemekFiyati")
+
+    val marka = "nike"
+    println("marka: $marka")
+
+    val muzikAdi = "dilektasi"
+    println("muzikAdi: $muzikAdi")
+
+    val videoSuresi = "2.14.38"
+    println("videoSuresi: $videoSuresi")
+
+    var urunPuani = "5/5"
+    println("urunPuani: $urunPuani")
+
+    var resimAdi = "seyehat"
+    println("resimAdi: $resimAdi")
+
+    var dosyaFormati = "dosyaformati"
+    println("dosyaFormati: $dosyaFormati")
+
+    val renk = "siyah"
+    println("renk: $renk")
+
+    val renkKodu = "00000"
+    println("renkKodu: $renkKodu")
+
+    val telefonModeli = "iphone13"
+    println("telefonModeli: $telefonModeli")
+
+    val ekranBoyutu = "ekranBoyutu"
+    println("ekranBoyutu: $ekranBoyutu")
+
+    val agirlik = "15kg"
+    println("agirlik : $agirlik")
+
+    val ulusalgun = "23 Nisan"
+    println("ulusalgun : $ulusalgun")
+
+    val tatilGunu = "cumartesi"
+    println("tatilGunu : $tatilGunu")
+
+    val rezervasyonTarihi = "25/04/2024"
+    println("rezervasyonTarihi : $rezervasyonTarihi")
+
+    val sokakAdi = "Havuz Sk."
+    println("sokakAdi : $sokakAdi")
+
+    val otobusHatti = "Umraniye-Kadikoy"
+    println("otobusHatti : $otobusHatti")
+
+    var kalanDakika = "25dk"
+    println("kalanDakika : $kalanDakika")
+
+    val takipKodu = 14414
+    println("takipKodu : $takipKodu")
+
+    val kuponSuresi = "13gun"
+    println("kuponSuresi : $kuponSuresi")
+
+    val kuponKodu = "17Xr5Y"
+    println("kuponKodu : $kuponKodu")
+
+    var faturaAdresi = "Umraniye"
+    println("faturaAdresi : $faturaAdresi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
